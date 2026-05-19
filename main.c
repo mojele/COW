76a2d44bf0830ab664d1ef109300308be33cad77
Mojele <mojele12345@gmail.com>
2026-05-19 09:38:21 +0900

    final commit

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main()
 {
-<<<<<<< HEAD
     printf("Hello Coww!");
-=======
+
     printf("Hello Cow! Im the origin!");
->>>>>>> 5c6113f4979435715448a1d125f20133584a1bd8
+    
     return 0;
 }
